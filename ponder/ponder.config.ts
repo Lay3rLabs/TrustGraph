a12addc9b6ee22e79831be79a77949cec2ba8e65
Noah Saso <6721426+NoahSaso@users.noreply.github.com>
2025-09-29 15:26:45 -0400

    update ponder to v0.13.8, no need for patch anymore

--- ponder/ponder.config.ts
+++ ponder/ponder.config.ts
@@ -12,39 +12,42 @@ export default createConfig({
     //   rpc: process.env.PONDER_RPC_URL_1,
     //   ws: process.env.PONDER_WS_URL_1,
     // },
-    // base: {
-    //   id: 8453,
-    //   rpc: process.env.PONDER_RPC_URL_8453,
-    //   ws: process.env.PONDER_WS_URL_8453,
-    // },
-    local: {
-      id: 31337,
-      rpc: "http://localhost:8545",
-      ws: "ws://localhost:8546",
+    base: {
+      id: 8453,
+      rpc: process.env.PONDER_RPC_URL_8453,
+      ws: process.env.PONDER_WS_URL_8453,
     },
+    // local: {
+    //   id: 31337,
+    //   rpc: "http://localhost:8545",
+    //   ws: "ws://localhost:8546",
+    // },
   },
   contracts: {
     marketMaker: {
       abi: lmsrMarketMakerAbi,
-      startBlock: 1,
+      // startBlock: "latest",
+      startBlock: 35855002,
       chain: {
-        local: {
-          address: deploymentSummary.prediction_market.market_maker as Hex,
-        },
+        // mainnet: { address: "0x0000000000000000000000000000000000000000" },
+        base: { address: deploymentSummary.prediction_market.market_maker as Hex },
+        // local: {
+        //   address: deploymentSummary.prediction_market.market_maker as Hex,
+        // },
       },
     },
     conditionalTokens: {
       abi: conditionalTokensAbi,
-      startBlock: 1,
+      startBlock: 35855002,
       chain: {
-        local: { address: deploymentSummary.prediction_market.conditional_tokens as Hex },
+        base: { address: deploymentSummary.prediction_market.conditional_tokens as Hex },
       },
     },
     wavsIndexer: {
       abi: wavsIndexerAbi,
-      startBlock: 1,
+      startBlock: 35855002,
       chain: {
-        local: { address: deploymentSummary.wavs_indexer as Hex },
+        base: { address: deploymentSummary.wavs_indexer as Hex },
       },
     }
   },
